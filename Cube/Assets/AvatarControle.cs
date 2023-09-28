c6e9a806b38d3a94b6eefeaa69de38e7906f4d13
STUDENT Vladislav Onishchenko <Vladislav.Onishchenko@students.ittralee.ie>
2023-09-28 11:00:13 +0100

    2 commit
    Signed-off-by: STUDENT Vladislav Onishchenko <Vladislav.Onishchenko@students.ittralee.ie>

---
+++ Cube/Assets/AvatarControle.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarControle : MonoBehaviour
+{
+    float currentSpeed, walkingSpeed = 2, runningSpeed = 4;
+    private float turningSpeed=180;
+    Animator MyAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = walkingSpeed;
+        MyAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyAnimator.SetBool("iswalking", false);
+
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            MyAnimator.SetBool("iswalking", true);
+            transform.position +=currentSpeed * transform.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= currentSpeed * transform.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up,turningSpeed* Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up,-turningSpeed * Time.deltaTime);
+        }
+
+    }
+}
